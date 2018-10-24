ddbefb386cecbfada6bb23d7843717b11b0dc931
Francisco J. Gomez Florido <fjgomezflorido@gmail.com>
2018-10-24 18:22:27 +0200

    Añadido ejercicio 7 y 10 y de la relación 3

---
+++ 1er Trimestre/Tema 3/App_R3E10_7A99EsPrimo/App_R3E10_7A99EsPrimo/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App_R3E10_7A99EsPrimo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduce número");
+            int numero = int.Parse(Console.ReadLine());
+            Console.WriteLine(EsPrimo(numero)); 
+            Console.ReadLine();
+        }
+        private static bool EsPrimo(int numero)
+        { 
+            bool esPrimo = true;
+            int i=2;
+            if (numero<2)
+	        {
+		        return false;
+	        }else if (numero<4)
+	        {
+                return true;
+            }
+            else
+            {
+                while(numero % i != 0 && i<=numero/2)
+                {
+                    i++;
+                }
+                return numero % i == 0 ? false : true;
+            }
+        }
+    }
+}
---
+++ 1er Trimestre/Tema 3/App_R3E7_ImprimeMayuscula/App_R3E7_ImprimeMayuscula/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App_R3E7_ImprimeMayuscula
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char teclaEscape = '*';
+            char teclaLeida;
+
+            do
+            {
+                teclaLeida = Console.ReadKey(true).KeyChar;
+                if(teclaLeida != teclaEscape)
+                {
+                    Console.Write(AMayuscula(teclaLeida).ToString());
+                }
+            } while(teclaLeida != teclaEscape);
+        }
+        private static char AMayuscula(char caracter)
+        {
+            int a = 97;
+            int A = 65;
+            int z = 122;
+            int diferenciaMayusMinus = a-A;
+            if((int)caracter >= a && (int)caracter <= z)
+            {
+                int caracterMayus = (int)caracter - diferenciaMayusMinus;
+                return (char)caracterMayus;
+            }
+            else if((int)caracter == 241) //CONTROL DE Ñ
+            {
+                return (char)209;
+            }
+            else if((int)caracter == 160) //CONTROL DE á
+            {
+                return (char)181;
+            }
+            else if((int)caracter == 130) //CONTROL DE é
+            {
+                return (char)144;
+            }
+            else if((int)caracter == 161)//CONTROL DE í
+            {
+                return (char)214;
+            }
+            else if((int)caracter == 161) //CONTROL DE ú
+            {
+                return (char)214;
+            }
+            else
+            {                      //PARA TODO LO DEMÁS MASTERCARD
+                return caracter;
+            }
+        }
+    }
+}
+
