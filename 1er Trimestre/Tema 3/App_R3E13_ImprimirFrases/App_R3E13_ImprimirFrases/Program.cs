4918d4d8e5b3f1f1ed24d269c03b4b206c45ed4f
Francisco J. Gomez Florido <fjgomezflorido@gmail.com>
2018-10-24 22:32:40 +0200

    Añadido de la relación 3 el ejercicio 13

---
+++ 1er Trimestre/Tema 3/App_R3E13_ImprimirFrases/App_R3E13_ImprimirFrases/Program.cs
@@ -0,0 +1,26 @@
+﻿/*-------------------------------------------------------------------------------------------------------------
+ *  Programa:		    Escribir frases
+ *  Autor:		        Francisco J. Gómez Florido
+ *  Versión:		    1.0 oct de 2018
+ *  Descripción:		Esta aplicación escribe frases con caracteres especiales
+-------------------------------------------------------------------------------------------------------------*/
+using System;
+
+namespace App_R3E13_ImprimirFrases
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Este programa escribe frases con caracteres especiales");
+            Console.WriteLine("Las comillas dobles( \" ) son especiales dentro del método Write. ");
+            Console.WriteLine("Esto es una barra invertida \\. ");
+            Console.WriteLine("El carácter \"\\t\" es tabulación horizontal ");
+            Console.WriteLine("El carácter \"\\n\" es salto de línea");
+            Console.WriteLine("Los caracteres {} son especiales");
+            int i = 5;
+            Console.WriteLine(" El primer valor se sustituye por {0} y vale: "+i);
+            Console.ReadLine();
+        }
+    }
+}
