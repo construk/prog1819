bcf762c9237ac2072ece758c2124c27070ff86a6
Francisco J. Gomez Florido <fjgomezflorido@gmail.com>
2018-10-25 19:28:19 +0200

    Añadido de la relación 3 el ejercicio 19 - MillaMarinaMetros

---
+++ 1er Trimestre/Tema 3/App_R3E19_MillaMarinaMetros/App_R3E19_MillaMarinaMetros/Program.cs
@@ -0,0 +1,25 @@
+﻿/*-------------------------------------------------------------------------------------------------------------
+ *  Programa:		    MillaMarinaMetros
+ *  Autor:		        Francisco J. Gómez Florido
+ *  Versión:		    1.0 oct de 2018
+ *  Descripción:		Esta aplicación sirve para calcular en metros las millas marinas
+-------------------------------------------------------------------------------------------------------------*/
+using System;
+
+namespace App_R3E19_MillaMarinaMetros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const double millaMarMetros = 1.852;
+            double millaMarina = 0;
+
+            Console.WriteLine("Este algoritmo transforma las millas marinas en metros");
+            Console.Write("Introduce millas marinas: ");
+            millaMarina = double.Parse(Console.ReadLine());
+            Console.WriteLine(millaMarina+" millas marinas son "+millaMarina*millaMarMetros+" metros.");
+            Console.ReadLine();
+        }
+    }
+}
