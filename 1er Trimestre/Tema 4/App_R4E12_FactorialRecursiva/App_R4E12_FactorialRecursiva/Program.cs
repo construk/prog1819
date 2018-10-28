a492f4313320a263f3d6f16b99fd83e3a5394b47
Francisco J. Gomez Florido <fjgomezflorido@gmail.com>
2018-10-28 22:02:20 +0100

    Añadido de la relación 4 el ejercicio 12 y 13 - Factorial Recursiva e Iterativa

---
+++ 1er Trimestre/Tema 4/App_R4E12_FactorialRecursiva/App_R4E12_FactorialRecursiva/Program.cs
@@ -0,0 +1,59 @@
+﻿/*-------------------------------------------------------------------------------------------------------------
+ *  Programa:		    Factorial Recursiva
+ *  Autor:		        Francisco J. Gómez Florido
+ *  Versión:		    1.0 oct de 2018
+ *  Descripción:		Esta aplicación haya el factorial de un número de manera recursiva.
+-------------------------------------------------------------------------------------------------------------*/
+using System;
+
+namespace App_R4E12_FactorialRecursiva
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double numero;
+            Console.WriteLine("Este programa calcula el factorial de cualquier número");
+            numero = uint.Parse(Console.ReadLine());
+            Console.WriteLine("{0:0,0}", FactorialRecursiva(numero));
+            Console.ReadLine();
+        }
+
+        //EL FACTORIAL MAYOR ACEPTADO CON DECIMAL ES 27, CON DOUBLE 170
+        public static uint FactorialRecursiva(uint numero)
+        {
+            if (numero <= 1)
+                return 1;
+            else
+                return numero * FactorialRecursiva(numero - 1);
+        }
+        public static ulong FactorialRecursiva(ulong numero)
+        {
+            if (numero <= 1)
+                return 1;
+            else
+                return numero * FactorialRecursiva(numero - 1);
+        }
+        public static decimal FactorialRecursiva(decimal numero)
+        {
+            if (numero <= 1)
+                return 1;
+            else
+                return numero * FactorialRecursiva(numero - 1);
+        }
+        public static double FactorialRecursiva(double numero)
+        {
+            if (numero <= 1)
+                return 1;
+            else
+                return numero * FactorialRecursiva(numero - 1);
+        }
+        public static float FactorialRecursiva(float numero)
+        {
+            if (numero <= 1)
+                return 1;
+            else
+                return numero * FactorialRecursiva(numero - 1);
+        }
+    }
+}
---
+++ 1er Trimestre/Tema 4/App_R4E13_FactorialIterativa/App_R4E13_FactorialIterativa/Program.cs
@@ -0,0 +1,88 @@
+﻿/*-------------------------------------------------------------------------------------------------------------
+ *  Programa:		    Factorial Iterativa
+ *  Autor:		        Francisco J. Gómez Florido
+ *  Versión:		    1.0 oct de 2018
+ *  Descripción:		Esta aplicación haya el factorial de un número de manera iterativa.
+-------------------------------------------------------------------------------------------------------------*/
+using System;
+
+
+namespace App_R4E13_FactorialIterativa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long numero;
+
+            Console.Write("Introduce un número:");
+            numero = long.Parse(Console.ReadLine());
+            Console.WriteLine("{0:0,0}", FactorialIterativa(numero));
+            Console.ReadLine();
+        }
+        public static double FactorialIterativa(double numero)
+        {
+            int factorial = 1;
+            if (numero == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                for (int i = 1; i <= numero; i++)
+                {
+                    factorial *= i;
+                }
+                return factorial;
+            }
+        }
+        public static decimal FactorialIterativa(decimal numero)
+        {
+            int factorial = 1;
+            if (numero == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                for (int i = 1; i <= numero; i++)
+                {
+                    factorial *= i;
+                }
+                return factorial;
+            }
+        }
+        public static long FactorialIterativa(long numero)
+        {
+            int factorial = 1;
+            if (numero == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                for (int i = 1; i <= numero; i++)
+                {
+                    factorial *= i;
+                }
+                return factorial;
+            }
+        }
+        public static float FactorialIterativa(float numero)
+        {
+            int factorial = 1;
+            if (numero == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                for (int i = 1; i <= numero; i++)
+                {
+                    factorial *= i;
+                }
+                return factorial;
+            }
+        }
+    }
+}
