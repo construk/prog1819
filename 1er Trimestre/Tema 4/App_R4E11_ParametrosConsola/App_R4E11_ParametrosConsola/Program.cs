48b2e78fd98631dc7c7068afa5e046cf7b4db5b8
Francisco J. Gomez Florido <fjgomezflorido@gmail.com>
2018-10-28 21:30:45 +0100

    Añadido de la relación 4 el ejercicio 11 - Parametros Consola

---
+++ 1er Trimestre/Tema 4/App_R4E11_ParametrosConsola/App_R4E11_ParametrosConsola/Program.cs
@@ -0,0 +1,22 @@
+﻿/*-------------------------------------------------------------------------------------------------------------
+ *  Programa:		    ParametrosConsola
+ *  Autor:		        Francisco J. Gómez Florido
+ *  Versión:		    1.0 oct de 2018
+ *  Descripción:		Esta aplicación escribe en pantalla todos los parametros introducidos por consola.
+-------------------------------------------------------------------------------------------------------------*/
+using System;
+
+namespace App_R4E11_ParametrosConsola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < args.Length; i++)       //BUCLE QUE RECORRE TODOS LOS ARGUMENTOS INTRODUCIDOS POR CONSOLA
+            {
+                Console.WriteLine(args[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
