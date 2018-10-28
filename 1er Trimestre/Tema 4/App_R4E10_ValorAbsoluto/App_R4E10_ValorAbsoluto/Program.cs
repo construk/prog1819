aa7783216537663c48a3e00f4591ec37f4489536
Francisco J. Gomez Florido <fjgomezflorido@gmail.com>
2018-10-28 21:25:11 +0100

    Añadidos de la relación 4 los ejercicios 9 y 10 - Media Notas y Valor Absoluto

---
+++ 1er Trimestre/Tema 4/App_R4E10_ValorAbsoluto/App_R4E10_ValorAbsoluto/Program.cs
@@ -0,0 +1,79 @@
+﻿/*-------------------------------------------------------------------------------------------------------------
+ *  Programa:		    ValorAbsoluto
+ *  Autor:		        Francisco J. Gómez Florido
+ *  Versión:		    1.0 oct de 2018
+ *  Descripción:		Esta aplicación contiene distintas sobrecargas para obtener el valor absoluto de cualquier número.
+-------------------------------------------------------------------------------------------------------------*/
+using System;
+
+namespace Cons
+{
+    class Math
+    {
+        static void Main(string[] args)
+        {
+            double numero;
+            string auxiliar;
+            Console.WriteLine("La función ValorAbsoluto te devuelve cualquier número en valor absoluto");
+            do
+            {
+                Console.Write("Introduce número (0 para salir): ");
+                auxiliar = Console.ReadLine();
+                while (!double.TryParse(auxiliar, out numero))
+                {
+                    Console.Write("Introduce un número válido: ");
+                    auxiliar = Console.ReadLine();
+                }
+                if (numero!=0)
+                {
+                    Console.WriteLine("El valor absoluto de "+numero+" es "+ValorAbsoluto(numero));
+                }
+            } while (numero!=0);
+        }
+
+        //Distintas sobrecargas para el método ValorAbsoluto
+        public static double ValorAbsoluto(double numero)
+        {
+            if (numero >= 0)
+                return numero;
+            else
+                return -numero;
+        }
+        public static int ValorAbsoluto(int numero)
+        {
+            if (numero >= 0)
+                return numero;
+            else
+                return -numero;
+        }
+        public static decimal ValorAbsoluto(decimal numero)
+        {
+            if (numero >= 0)
+                return numero;
+            else
+                return -numero;
+        }
+        public static float ValorAbsoluto(float numero)
+        {
+            if (numero >= 0)
+                return numero;
+            else
+                return -numero;
+        }
+        public static long ValorAbsoluto(long numero)
+        {
+            if (numero >= 0)
+                return numero;
+            else
+                return -numero;
+        }
+        public static sbyte ValorAbsoluto(sbyte numero)
+        {
+            if (numero >= 0)
+                return numero;
+            else
+                return (sbyte)-numero;
+        }
+        
+    }
+}
---
+++ 1er Trimestre/Tema 4/App_R4E9_MediaNotas/App_R4E9_MediaNotas/Program.cs
@@ -0,0 +1,43 @@
+﻿/*-------------------------------------------------------------------------------------------------------------
+ *  Programa:		    Media Notas
+ *  Autor:		        Francisco J. Gómez Florido
+ *  Versión:		    1.0 oct de 2018
+ *  Descripción:		Esta aplicación calcula la media de las notas introducidas.
+-------------------------------------------------------------------------------------------------------------*/
+using System;
+
+namespace App_R4E9_MediaNotas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double notaLeida;
+            double sumaNotas = 0;
+            int contador = 0;
+            string auxiliar;
+            double media;
+
+            Console.WriteLine("Esta aplicación calcula la media de las notas introducidas.");
+            do
+            {
+                Console.Write("Introduce una nota: ");
+                auxiliar = Console.ReadLine();
+                while (!double.TryParse(auxiliar, out notaLeida))
+                {
+                    Console.Write("Introduce una nota válida: ");
+                    auxiliar = Console.ReadLine();
+                }
+                if (notaLeida!=0)
+                {
+                    sumaNotas += notaLeida;
+                    contador++;
+                }
+            } while (notaLeida != 0);
+
+            media = sumaNotas / contador;
+            Console.WriteLine("\nLa media de las {0} notas es {1:F2}",contador,media);
+            Console.ReadLine();
+        }
+    }
+}
