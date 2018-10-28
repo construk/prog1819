f78c5fcb6e8da90acba9809f335fc6f0b4299db6
Francisco J. Gomez Florido <fjgomezflorido@gmail.com>
2018-10-28 17:52:17 +0100

    Añadido de la relación 4 el ejercicio 4 - Es primo

---
+++ 1er Trimestre/Tema 4/App_R4E4_EsPrimo/App_R4E4_EsPrimo/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace Cons
+{
+    class Math
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(EsPrimo(-7));
+            Console.WriteLine(EsPrimo(-8));
+            Console.WriteLine(EsPrimo(-11));
+            Console.WriteLine(EsPrimo(-12));
+            Console.WriteLine(EsPrimo(-13));
+            Console.WriteLine(EsPrimo(-773));
+            Console.WriteLine(EsPrimo(-733));
+            Console.ReadLine();
+        }
+        //FUNCIÓN QUE DEVUELVE TRUE SI ES PRIMO Y FALSE SI NO LO ES.
+        public static bool EsPrimo(uint numero) //Función que recibe un número de tipo uint que devuelve true si es primo y false si no lo es
+        {
+            int divisor = 2;      //Desde donde comenzamos a comprobar divisibles
+            if (numero < 2)       //Controlamos el caso del número 0 y 1, que no son primos
+            {
+                return false;
+            }
+            else if (numero < 4) //Controlamos el caso del número 2 y 3, que son primos
+            {
+                return true;
+            }
+            else                //Controlamos el resto de casos (hasta el máximo aceptado por un uint)
+            {
+                while (numero % divisor != 0 && divisor <= numero / 2)   //Mientras el número no sea divisible y el divisor sea menor a la mitad del número comprobado, incrementa divisor
+                {                                                   //Cuando termine de comprobar las condiciones se evaluará el último valor contemplado como divisor
+                    divisor++;
+                }
+                return numero % divisor == 0 ? false : true;        //y si el numero entre el divisor es 0 será falso y si no será verdadero
+            }
+        }
+        public static bool EsPrimo(int numero) //Función que recibe un número de tipo int que devuelve true si es primo y false si no lo es
+        {
+            int divisor = 2;      //Desde donde comenzamos a comprobar divisibles
+            if (numero >= 0)
+            {
+                if (numero < 2)       //Controlamos el caso del número 0 y 1, que no son primos
+                {
+                    return false;
+                }
+                else if (numero < 4) //Controlamos el caso del número 2 y 3, que son primos
+                {
+                    return true;
+                }
+                else                //Controlamos el resto de casos (hasta el máximo aceptado por un uint)
+                {
+                    while (numero % divisor != 0 && divisor <= numero / 2)   //Mientras el número no sea divisible y el divisor sea menor a la mitad del número comprobado, incrementa divisor
+                    {                                                   //Cuando termine de comprobar las condiciones se evaluará el último valor contemplado como divisor
+                        divisor++;
+                    }
+                    return numero % divisor == 0 ? false : true;        //y si el numero entre el divisor es 0 será falso y si no será verdadero
+                }
+            }
+            else {
+                if (numero > -2)       //Controlamos el caso del número -1, que no es primo
+                {
+                    return false;
+                }
+                else if (numero > -4) //Controlamos el caso del número -2 y -3, que son primos
+                {
+                    return true;
+                }
+                else                //Controlamos el resto de casos (hasta el máximo aceptado por un int)
+                {
+                    while (numero % divisor != 0 && divisor <= -numero / 2)   //Mientras el número no sea divisible y el divisor sea menor a la mitad del número comprobado, incrementa divisor
+                    {                                                   //Cuando termine de comprobar las condiciones se evaluará el último valor contemplado como divisor
+                        divisor++;
+                    }
+                    return numero % divisor == 0 ? false : true;        //y si el numero entre el divisor es 0 será falso y si no será verdadero
+                }
+            }
+        }
+    }
+}
