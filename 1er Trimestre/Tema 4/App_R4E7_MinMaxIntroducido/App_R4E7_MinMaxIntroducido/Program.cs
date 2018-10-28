c69a218aa3f9e012e25f7276660825dc7175e56f
Francisco J. Gomez Florido <fjgomezflorido@gmail.com>
2018-10-28 20:48:01 +0100

    Añadido de la relación 4 el ejercicio 7 y 8 - Min Max Introducido y Mayus

---
+++ 1er Trimestre/Tema 4/App_R4E7_MinMaxIntroducido/App_R4E7_MinMaxIntroducido/Program.cs
@@ -0,0 +1,55 @@
+﻿/*-------------------------------------------------------------------------------------------------------------
+ *  Programa:		    MinMaxIntroducido
+ *  Autor:		        Francisco J. Gómez Florido
+ *  Versión:		    1.0 oct de 2018
+ *  Descripción:		Aplicación que te dice el número más grande introducido y el más pequeño
+-------------------------------------------------------------------------------------------------------------*/
+using System;
+
+namespace App_R4E7_MinMaxIntroducido
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //CAMPOS
+            double max=0;
+            double min=0;
+            string auxiliar;
+            double numero;
+            
+            //INTRODUCCIÓN
+            Console.WriteLine("Introduce números y te mostraré el número más grande y más pequeño introducido");
+            do  //HACER MIENTRAS NÚMERO SEA DISTINTO DE 0
+            {
+                //INTRODUCIR DATOS
+                Console.Write("Introduce un número (0 para salir): ");
+                auxiliar = Console.ReadLine();
+                while (!double.TryParse(auxiliar, out numero))
+                {
+                    Console.Write("Introduce un número válido: ");
+                    auxiliar = Console.ReadLine();
+                }
+
+                if (numero!=0) //SI NÚMERO ES DISTINTO DE CERO, COMPARAR PARA ASIGNAR A MAX O MIN
+                {
+                    if (numero<min)
+                        min = numero;
+                    if (numero>max)
+                        max = numero;
+                }
+            } while (numero!=0);
+            
+            //RESULTADOS
+            if (min==0&&max==0)
+                Console.WriteLine("No has introducido ningún número");
+            else if (min==0)
+                Console.WriteLine("El valor máximo es {0}",max);
+            else if (max==0)
+                Console.WriteLine("El valor mínimo es {0}",min);
+            else
+                Console.WriteLine("El valor máximo es {0}\nEl valor mínimo es {1}",max,min);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 1er Trimestre/Tema 4/App_R4E8_Mayus/App_R4E8_Mayus/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+
+namespace App_R4E8_Mayus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleKeyInfo tecla = new ConsoleKeyInfo();
+            Console.WriteLine("Programa que transforma todo lo que escribas a mayúsculas");
+            Console.WriteLine("* para salir");
+            
+            do
+            {
+                Console.Write(Mayus(Console.ReadKey(true)));
+            } while (tecla.KeyChar!='*');
+
+        }
+        public static char Mayus(ConsoleKeyInfo tecla)
+        {
+            int diferenciaMayusMinus = (int)'a' - (int)'A';
+            
+            if (tecla.KeyChar>='a'&&tecla.KeyChar<='z')//RANGO a-z
+            {
+                return (char)(tecla.KeyChar - diferenciaMayusMinus);
+            }
+            else if (tecla.KeyChar == 'ñ')//ñ
+            {
+                return 'Ñ';
+            }
+            else if (tecla.KeyChar=='á')//´
+            {
+                return 'Á';
+            }
+            else if (tecla.KeyChar == 'é')
+            {
+                return 'É';
+            }
+            else if (tecla.KeyChar == 'í')
+            {
+                return 'Í';
+            }
+            else if (tecla.KeyChar == 'ó')
+            {
+                return 'Ó';
+            }
+            else if (tecla.KeyChar == 'ú')
+            {
+                return 'Ú';
+            }
+            else if (tecla.KeyChar == 'à')//`
+            {
+                return 'À';
+            }
+            else if (tecla.KeyChar == 'è')
+            {
+                return 'È';
+            }
+            else if (tecla.KeyChar == 'ì')
+            {
+                return 'Ì';
+            }
+            else if (tecla.KeyChar == 'ò')
+            {
+                return 'Ò';
+            }
+            else if (tecla.KeyChar == 'ù')
+            {
+                return 'Ù';
+            }
+            else if (tecla.KeyChar == 'ä')//¨
+            {
+                return 'Ä';
+            }
+            else if (tecla.KeyChar == 'ë')
+            {
+                return 'Ë';
+            }
+            else if (tecla.KeyChar == 'ï')
+            {
+                return 'Ï';
+            }
+            else if (tecla.KeyChar == 'ö')
+            {
+                return 'Ö';
+            }
+            else if (tecla.KeyChar == 'ü')
+            {
+                return 'Ü';
+            }
+            else if (tecla.KeyChar == 'ç')
+            {
+                return 'Ç';
+            }
+            else
+            {
+                return tecla.KeyChar;
+            }
+        }
+    }
+}
