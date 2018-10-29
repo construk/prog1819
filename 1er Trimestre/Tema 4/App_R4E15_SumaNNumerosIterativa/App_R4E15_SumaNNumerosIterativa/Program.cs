ecdbca4af52a101b0057a3391f50db5628bf31a5
Francisco J. Gómez Florido <fjgomezflorido@gmail.com>
2018-10-29 09:08:25 +0100

    Añadido de la relación 4 el ejercicio 15 - Suma N Numeros Iterativa

---
+++ 1er Trimestre/Tema 4/App_R4E15_SumaNNumerosIterativa/App_R4E15_SumaNNumerosIterativa/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace App_R4E15_SumaNNumerosIterativa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string aux;
+            long numero;
+            Console.WriteLine("Programa que hace la suma gaussiana");
+            do
+            {
+                Console.Write("Introduce un número (0 para salir): ");
+                aux = Console.ReadLine();
+                while(!long.TryParse(aux,out numero))
+                {
+                    Console.Write("Introduce un número válido: ");
+                    aux=Console.ReadLine();
+                }
+                Console.WriteLine(SumaNNumerosIterativa(numero));
+            } while(numero!=0);
+        }
+        public static long SumaNNumerosIterativa(long sumarHasta)
+        {
+            long resultado = 0;
+            for(int i = 1; i <= sumarHasta; i++)
+            {
+                resultado += i;
+            }
+            return resultado;
+        }
+    }
+}
