eebcfc5b8fac75a7b831b1c33602eed231b5bdbc
Francisco J. Gomez Florido <fjgomezflorido@gmail.com>
2018-10-30 19:50:51 +0100

    Añadido de la relación 4 el ejercicio 16 - Potencia Recursiva

---
+++ 1er Trimestre/Tema 4/App_R4E16_PotenciaRecursiva/App_R4E16_PotenciaRecursiva/Program.cs
@@ -0,0 +1,57 @@
+﻿/*-------------------------------------------------------------------------------------------------------------
+ *  Programa:		    PotenciaRecursiva
+ *  Autor:		        Francisco J. Gómez Florido
+ *  Versión:		    1.0 oct de 2018
+ *  Descripción:		Esta aplicación calcula la potencia de un número de forma recursiva
+-------------------------------------------------------------------------------------------------------------*/
+using System;
+
+namespace App_R4E16_PotenciaRecursiva
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //VARIABLES
+            int numero;
+            int exponente;
+            string aux;
+            
+            //INTRODUCCIÓN
+            Console.WriteLine("Programa que calcula la potencia de un número");
+            
+            //LEER DATOS
+            Console.Write("Introduce la base: ");
+            aux = Console.ReadLine();
+            while (!int.TryParse(aux, out numero))
+            {
+                Console.Write("Introduce un número válido: ");
+                aux = Console.ReadLine();
+            }
+            aux = null;
+            Console.Write("Introduce el exponente: ");
+            aux = Console.ReadLine();
+            while (!int.TryParse(aux, out exponente))
+            {
+                Console.Write("Introduce un número válido: ");
+                aux = Console.ReadLine();
+            }
+            
+            //APLICANDO LA FÚNCIÓN RECURSIVA
+            Console.WriteLine(PotenciaRecursiva(numero, exponente));
+            Console.ReadLine();
+
+        }
+        public static double PotenciaRecursiva(int numero, int exponente)
+        {
+            if (exponente == 0) //Caso base: Cuando el exponente es 0 siempre devuelve 1
+            {
+                return 1;
+            }
+            else    
+            {
+                return numero * PotenciaRecursiva(numero, exponente - 1);
+            }
+        }
+    }
+}
