c971c40ab1b5ff68637f0e371d1d6d5951624d73
Francisco J. Gomez Florido <fjgomezflorido@gmail.com>
2018-11-02 16:45:27 +0100

    Añadida excepción personalizada

--- 1er Trimestre/Tema 4/App_R4E19_FibonacciRecursivo/App_R4E19_FibonacciRecursivo/Program.cs
+++ 1er Trimestre/Tema 4/App_R4E19_FibonacciRecursivo/App_R4E19_FibonacciRecursivo/Program.cs
@@ -8,6 +8,23 @@ using System;
 
 namespace App_R4E19_FibonacciRecursivo
 {
+    public class NoNegativeFibonacciException : Exception
+    {
+        private string mensajeError = "ERROR: No se puede obtener el número de la sucesión de Fibonacci negativo";
+
+        public override string Message
+        {
+            get { return mensajeError; }
+        }
+        public NoNegativeFibonacciException()
+        {
+        }
+        public NoNegativeFibonacciException(string message) : base(message)
+        {
+            mensajeError = message;
+        }
+
+    }
     class Program
     {
         static void Main(string[] args)
@@ -15,7 +32,7 @@ namespace App_R4E19_FibonacciRecursivo
             //VARIABLES
             string aux;
             int numero;
-            
+
             //INTRODUCCIÓN
             Console.WriteLine("Programa que calcula el número de la sucesión de Fibonacci que indiques (-1 para salir)");
             do
@@ -29,21 +46,37 @@ namespace App_R4E19_FibonacciRecursivo
                     aux = Console.ReadLine();
                 }
                 //ESCRIBE EL NÚMERO DE LA SUCESIÓN DE FIBONACCI INDICADO
-                Console.WriteLine(FibonacciRecursivo(numero));
-            } while (numero!=-1);
+                try
+                {
+                    if (numero!=-1)
+                    {
+                        Console.WriteLine(FibonacciRecursivo(numero));
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            } while (numero != -1);
+            Console.WriteLine("Pulse ENTER para salir...");
+            Console.ReadLine();
         }
 
         public static int FibonacciRecursivo(int numero) //FUNCIÓN QUE TE DEVUELVE EL VALOR DE LA SUCESIÓN DE FIBONACCI QUE LE INDIQUES
         {
-            if (numero==0) //CASO BASE 1: 0
+            if (numero < 0)
+            {
+                throw new NoNegativeFibonacciException();
+            }
+            else if (numero == 0) //CASO BASE 1: 0
             {
                 return 0;
             }
-            else if (numero==1) //CASO BASE 2: 1
+            else if (numero == 1) //CASO BASE 2: 1
             {
                 return 1;
             }
-            else            
+            else
             {
                 return FibonacciRecursivo(numero - 1) + FibonacciRecursivo(numero - 2);
             }
