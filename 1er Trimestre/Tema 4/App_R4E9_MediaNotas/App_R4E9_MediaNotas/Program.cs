7c2b4cc59329e85c5a12f73cb759b187f90ec28d
Francisco J. Gomez Florido <fjgomezflorido@gmail.com>
2018-11-02 11:15:24 +0100

    Mejora en el código y comentado

--- 1er Trimestre/Tema 4/App_R4E9_MediaNotas/App_R4E9_MediaNotas/Program.cs
+++ 1er Trimestre/Tema 4/App_R4E9_MediaNotas/App_R4E9_MediaNotas/Program.cs
@@ -17,26 +17,32 @@ namespace App_R4E9_MediaNotas
             int contador = 0;
             string auxiliar;
             double media;
-
+            //INTRODUCCIÓN
             Console.WriteLine("Esta aplicación calcula la media de las notas introducidas.");
-            do
+            do //Hacer mientras el número introducido sea distinto de 0
             {
+                //LEER DATOS
                 Console.Write("Introduce una nota: ");
                 auxiliar = Console.ReadLine();
-                while (!double.TryParse(auxiliar, out notaLeida))
+                while (!double.TryParse(auxiliar, out notaLeida)||notaLeida<0)
                 {
                     Console.Write("Introduce una nota válida: ");
                     auxiliar = Console.ReadLine();
                 }
-                if (notaLeida!=0)
+                //COMPROBACIONES
+                if (notaLeida!=0) //SI NOTA LEIDA ES DISTINTA DE 0, SUMARLA Y AUMENTAR EL CONTADOR
                 {
                     sumaNotas += notaLeida;
                     contador++;
                 }
+                else if (contador!=0) //SI EL CONTADOR ES DISTINTO DE 0 (SI SALE METIENDO DATOS) TE DICE LA MEDIA
+                {
+                    media = sumaNotas / contador;
+                    Console.WriteLine("\nLa media de las {0} notas es {1:F2}", contador, media);
+                }
             } while (notaLeida != 0);
-
-            media = sumaNotas / contador;
-            Console.WriteLine("\nLa media de las {0} notas es {1:F2}",contador,media);
+            //MENSAJE FIN DE PROGRAMA
+            Console.WriteLine("Pulsa ENTER para salir...");
             Console.ReadLine();
         }
     }
