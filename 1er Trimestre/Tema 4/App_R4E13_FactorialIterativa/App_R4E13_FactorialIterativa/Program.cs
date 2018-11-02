515eab016ae00cecda28fd25416497df8bb68d7a
Francisco J. Gomez Florido <fjgomezflorido@gmail.com>
2018-11-02 13:01:16 +0100

    Añadida excepción personalizada y controlados negativos

--- 1er Trimestre/Tema 4/App_R4E13_FactorialIterativa/App_R4E13_FactorialIterativa/Program.cs
+++ 1er Trimestre/Tema 4/App_R4E13_FactorialIterativa/App_R4E13_FactorialIterativa/Program.cs
@@ -9,21 +9,61 @@ using System;
 
 namespace App_R4E13_FactorialIterativa
 {
+    /// <summary>
+    /// Excepción producida cuando intentas realizar el factorial de un número negativo
+    /// </summary>
+    public class FactorialNoNegativeException : Exception
+    {
+        private string mensajeExcepcion = "ERROR: No se puede calcular el factorial de un número negativo";
+
+        public override string Message
+        { get { return mensajeExcepcion; } }
+
+        public FactorialNoNegativeException()
+        {
+            ConsoleColor colorTexto = new ConsoleColor();
+            Console.ForegroundColor = colorTexto;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(Message);
+            Console.ForegroundColor = colorTexto;
+        }
+
+        public FactorialNoNegativeException(string message) : base(message)
+        {
+            ConsoleColor colorTexto = new ConsoleColor();
+            Console.ForegroundColor = colorTexto;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(message);
+            Console.ForegroundColor = colorTexto;
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
             double numero;
-
-            Console.Write("Introduce un número:");
-            numero = double.Parse(Console.ReadLine());
-            Console.WriteLine("{0:0,0}", FactorialIterativa(numero));
+            Console.WriteLine("Programa que calcula el factorial de forma iterativa");
+            Console.Write("Introduce un número: ");
+            try
+            {
+                numero = double.Parse(Console.ReadLine());
+                Console.WriteLine("{0:0,0}", FactorialIterativa(numero));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message); ;
+            }
             Console.ReadLine();
         }
         public static double FactorialIterativa(double numero)
         {
             double factorial = 1;
-            if (numero == 0)
+            if (numero<0)
+            {
+                throw new FactorialNoNegativeException();
+            }
+            else if (numero == 0)
             {
                 return 1;
             }
@@ -55,7 +95,11 @@ namespace App_R4E13_FactorialIterativa
         public static long FactorialIterativa(long numero)
         {
             long factorial = 1;
-            if (numero == 0)
+            if (numero < 0)
+            {
+                throw new FactorialNoNegativeException();
+            }
+            else if (numero == 0)
             {
                 return 1;
             }
@@ -71,7 +115,11 @@ namespace App_R4E13_FactorialIterativa
         public static float FactorialIterativa(float numero)
         {
             float factorial = 1;
-            if (numero == 0)
+            if (numero < 0)
+            {
+                throw new FactorialNoNegativeException();
+            }
+            else if (numero == 0)
             {
                 return 1;
             }
