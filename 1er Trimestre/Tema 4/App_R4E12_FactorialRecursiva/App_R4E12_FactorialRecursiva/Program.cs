a949a3299890255166f65694a32b585a99d82e43
Francisco J. Gomez Florido <fjgomezflorido@gmail.com>
2018-11-02 12:54:21 +0100

    Añadida excepción personalizada y controlado negativos en sobrecarga con double

--- 1er Trimestre/Tema 4/App_R4E12_FactorialRecursiva/App_R4E12_FactorialRecursiva/Program.cs
+++ 1er Trimestre/Tema 4/App_R4E12_FactorialRecursiva/App_R4E12_FactorialRecursiva/Program.cs
@@ -8,14 +8,51 @@ using System;
 
 namespace App_R4E12_FactorialRecursiva
 {
+    /// <summary>
+    /// Excepción producida cuando intentas realizar el factorial de un número negativo
+    /// </summary>
+    public class FactorialNoNegativeException : Exception
+    {
+        private string mensajeExcepcion = "ERROR: No se puede calcular el factorial de un número negativo";
+
+        public override string Message
+        { get { return mensajeExcepcion; } }
+
+        public FactorialNoNegativeException()
+        {
+            ConsoleColor colorTexto = new ConsoleColor();
+            Console.ForegroundColor = colorTexto;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(Message);
+            Console.ForegroundColor = colorTexto;
+        }
+
+        public FactorialNoNegativeException(string message) : base(message)
+        {
+            ConsoleColor colorTexto = new ConsoleColor();
+            Console.ForegroundColor = colorTexto;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(message);
+            Console.ForegroundColor = colorTexto;
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
             double numero;
             Console.WriteLine("Este programa calcula el factorial de cualquier número");
-            numero = uint.Parse(Console.ReadLine());
-            Console.WriteLine("{0:0,0}", FactorialRecursiva(numero));
+            try
+            {
+                Console.Write("Introduce un número entero no negativo: ");
+                numero = double.Parse(Console.ReadLine());
+                Console.WriteLine("{0:0,0}", FactorialRecursiva(numero));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
             Console.ReadLine();
         }
 
@@ -41,8 +78,17 @@ namespace App_R4E12_FactorialRecursiva
             else
                 return numero * FactorialRecursiva(numero - 1);
         }
+        /// <summary>
+        /// Función que calcula el factorial de un número. 
+        /// </summary>
+        /// <param name="numero">Numero sobre el que calcular el factorial. Si se introduce un número negativo produce FactorialNoNegativeException</param>
+        /// <returns>Devuelve el factorial como double</returns>
         public static double FactorialRecursiva(double numero)
         {
+            if (numero < 0)
+            {
+                throw new FactorialNoNegativeException();
+            }
             if (numero <= 1)
                 return 1;
             else
